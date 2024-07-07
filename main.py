ae88c781514de419f368b6aca319aad0176df610
oratiek <oratiek522gmail.com>
2024-07-07 21:16:33 +0900

    made judge function clean

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ import random
 hands = ["ぐー","ちょき","ぱー"]
 
 def judge(ene_hand, usr_hand):
-    diff = usr_hand - ene_hand if (usr_hand - ene_hand)>= 0 else (usr_hand - ene_hand)+ 3
-    if diff == 0:
-        return "draw"
-    elif diff  == 1:
-        return "lose"
-    elif diff == 2:
-        return "win"
+    diff = usr_hand - ene_hand if (usr_hand - ene_hand)>= 0 else (usr_hand - ene_hand) + 3
+    if   diff == 0: return "draw"
+    elif diff == 1: return "lose"
+    elif diff == 2: return "win"
 
 if __name__ == "__main__":
     ene_hand = random.randint(0,2)
