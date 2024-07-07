06ca26550933ee1f3a8208442b7819b05e944bfa
oratiek <oratiek522gmail.com>
2024-07-07 21:17:29 +0900

    add space to make it easier to read

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 hands = ["ぐー","ちょき","ぱー"]
 
 def judge(ene_hand, usr_hand):
-    diff = usr_hand - ene_hand if (usr_hand - ene_hand)>= 0 else (usr_hand - ene_hand) + 3
+    diff = usr_hand - ene_hand if (usr_hand - ene_hand) >= 0 else (usr_hand - ene_hand) + 3
     if   diff == 0: return "draw"
     elif diff == 1: return "lose"
     elif diff == 2: return "win"
